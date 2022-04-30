c6682790e83508e03f49fe702c3c4da791be10ed
aIsInAligatorStRong <90213463+aIsInAligatorStRong@users.noreply.github.com>
2022-04-30 10:25:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=m15.imageClassifier('MobileNet',modelloaded);
+}
+
+function modelloaded(){
+  console.log('model has been loaded');
+
+}
+function draw() {
+  image(video,0,0,300,300);
+  classifier.classify(video,gotresult);
+}
+var pr='';
+
+function gotresult(error,results){
+  if(error) {
+    console.log(error);
+  }
+  else if((results[0].confidence>0.5)&&(pr!=results[0].label)) {
+    console.log(results);
+    pr=results[0].label;
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence
+  }
+}
+
+
+
+
